45c1107c8dd862c7b7040b377a18d29c08d40c40
Bless <77230702+Blessiscool@users.noreply.github.com>
2021-12-26 06:14:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def tokenjoiner(session,token,proxies,invite):
     elif resp.status_code == 429:
         Thread(target=tokenjoiner,args=(session,token,proxies,invite)).start()
     else:
-        print("Invalid invite or token")
+        print("Invalid Token")
 def checker(session,token,proxies):
     valid = open("tokens.txt","w")
     headers = {
@@ -24,39 +24,34 @@ def checker(session,token,proxies):
     proxy = {"http":f"socks5://{next(proxies)}"}
     resp = session.get("https://canary.discord.com/api/v8/users/@me",headers = headers,proxies = proxy)
     if resp.status_code == 200:
-        valid.write(token + '\n')
-        print("{0} valid ".format(token))
+        valid.write(token + "\n")
+        print(f"{token} valid")
     elif resp.status_code == 429:
         Thread(target=checker,args=(session,token,proxies)).start()
-    else:
-        print("Invalid token")
+    
 def main():
     lock = threading.Lock()
     session = requests.Session()
     proxylist = open("proxies.txt","r").read().splitlines()
     proxies = cycle(proxylist)
     choice = input(" [1] Token Checker\n [2] Token Joiner\n [3] Exit\nChoice: ")
-    tokens = open("tokens.txt","r").read().splitlines()
-    if len(tokens) == 0:
-        print("Paste your tokens in tokens.txt")
-    else:
-        if choice == "1":
-            for user in tokens:
-                lock.acquire()
-                Thread(target=checker,args=(session,user,proxies)).start()
-                lock.release()
-            main()
-        elif choice == "2":
-            invite = input("Invite Code:  ")
-            for user in open("tokens.txt","r").read().splitlines():
+    if choice == "1":
+        for user in open("tokens.txt","r").read().splitlines():
+            lock.acquire()
+            Thread(target=checker,args=(session,user,proxies)).start()
+            lock.release()
+        main()
+    elif choice == "2":
+        invite = input("Invite Code:  ")
+        for user in open("tokens.txt","r").read().splitlines():
             try:
                 lock.acquire()
                 Thread(target=tokenjoiner,args=(session,user,proxies,invite)).start()
                 lock.release()
             except Exception as e:
-                print(e)
-            main()
-        else:
-            os._exit(1)
+                input(f"Error {e}\nPress Enter")
+        main()
+    else:
+        os._exit(1)
 if __name__ == "__main__":
     main()
